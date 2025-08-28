0f4b9fd2be3ef23234f1ea5e5893091fae4a6cb7
Amari Wilson <awilson84@husksers.unl.edu>
2025-08-28 14:43:05 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    printf("Amari Wilson\n");
+    printf("Amari Wilson!\n");
+    printf("My major is Computer Engineering");
 
     return 0;
 }
